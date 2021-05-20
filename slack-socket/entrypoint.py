7e79f8fb818f64b838fabe394bcd19328bc7b9ad
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-20 10:28:00 -0700

    clean up files

--- slack-socket/entrypoint.py
+++ slack-socket/entrypoint.py
@@ -9,16 +9,6 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).connect()
-# if os.environ["BLOCKS"]:
-#     app.client.chat_postMessage(token=os.environ["SLACK_BOT_TOKEN"],
-#                                 channel=os.environ["CHANNEL_ID"],
-#                                 text=os.environ["MESSAGE"],
-#                                 blocks=os.environ["BLOCKS"],
-#                                 attachments=os.environ["ATTACHMENTS"])
-# else:
-#     app.client.chat_postMessage(token=os.environ["SLACK_BOT_TOKEN"],
-#                                 channel=os.environ["CHANNEL_ID"],
-#                                 text=os.environ["MESSAGE"])
 
 app.client.chat_postMessage(token=os.environ["SLACK_BOT_TOKEN"],
                             channel=os.environ["CHANNEL_ID"],
