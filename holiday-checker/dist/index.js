579e6108d2c66a669d3c5e0d8dfa06cf45e0e39a
Kyle A. Matheny <kylematheny@protonmail.com>
2024-09-25 13:30:59 -0400

    pull dates out of js

--- holiday-checker/dist/index.js
+++ holiday-checker/dist/index.js
@@ -31626,29 +31626,10 @@ var __webpack_exports__ = {};
 const { formatInTimeZone } = __nccwpck_require__(7283);
 const core = __nccwpck_require__(5859);
 
-// Predefined list of holidays (format: YYYY-MM-DD)
-const holidays = [
-  "2024-01-15",
-  "2024-05-27",
-  "2024-06-19",
-  "2024-07-04",
-  "2024-09-02",
-  "2024-11-11",
-  "2024-11-28",
-  "2024-11-29",
-  "2024-12-23",
-  "2024-12-24",
-  "2024-12-25",
-  "2024-12-26",
-  "2024-12-27",
-  "2024-12-28",
-  "2024-12-29",
-  "2024-12-30",
-  "2024-12-31",
-  "2025-01-01",
-  "2024-09-25"
-];
-  
+// Git list of holidays
+const holidaysInput = core.getInput('holidays');
+const holidays = holidaysInput.split(',').map(date => date.trim());
+
 // Get the current date and time in EST Timezone  (YYYY-MM-DD)
 const today = new Date(); // This is in UTC
 const timeZone = 'America/New_York';
@@ -31661,11 +31642,10 @@ const isHoliday = holidays.includes(formattedDate);
 core.setOutput('is_holiday', isHoliday.toString());
 
 if (isHoliday) {
-  console.log(`Today (${formattedTime}) in ${timeZone} is a holiday.`);
+  console.log(`Today (${formattedTime}, ${timeZone}) is a holiday.`);
 } else {
-  console.log(`Today (${formattedTime}) in ${timeZone} is not a holiday.`);
+  console.log(`Today (${formattedTime},  ${timeZone} is not a holiday.`);
 }
-
 module.exports = __webpack_exports__;
 /******/ })()
 ;
--- holiday-checker/index.js
+++ holiday-checker/index.js
@@ -1,29 +1,10 @@
 const { formatInTimeZone } = require('date-fns-tz');
 const core = require('@actions/core');
 
-// Predefined list of holidays (format: YYYY-MM-DD)
-const holidays = [
-  "2024-01-15",
-  "2024-05-27",
-  "2024-06-19",
-  "2024-07-04",
-  "2024-09-02",
-  "2024-11-11",
-  "2024-11-28",
-  "2024-11-29",
-  "2024-12-23",
-  "2024-12-24",
-  "2024-12-25",
-  "2024-12-26",
-  "2024-12-27",
-  "2024-12-28",
-  "2024-12-29",
-  "2024-12-30",
-  "2024-12-31",
-  "2025-01-01",
-  "2024-09-25"
-];
-  
+// Git list of holidays
+const holidaysInput = core.getInput('holidays');
+const holidays = holidaysInput.split(',').map(date => date.trim());
+
 // Get the current date and time in EST Timezone  (YYYY-MM-DD)
 const today = new Date(); // This is in UTC
 const timeZone = 'America/New_York';
@@ -36,7 +17,7 @@ const isHoliday = holidays.includes(formattedDate);
 core.setOutput('is_holiday', isHoliday.toString());
 
 if (isHoliday) {
-  console.log(`Today (${formattedTime}) in ${timeZone} is a holiday.`);
+  console.log(`Today (${formattedTime}, ${timeZone}) is a holiday.`);
 } else {
-  console.log(`Today (${formattedTime}) in ${timeZone} is not a holiday.`);
+  console.log(`Today (${formattedTime},  ${timeZone} is not a holiday.`);
 }
